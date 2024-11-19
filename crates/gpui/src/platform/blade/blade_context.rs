0822e3d40bfaa3a246bc28f48368c4d1b334bf71
Dzmitry Malyshau <kvark@fastmail.com>
2024-11-19 00:01:40 -0800

    Switch to a single GPU context in Blade

---
+++ crates/gpui/src/platform/blade/blade_context.rs
@@ -0,0 +1,23 @@
+use blade_graphics as gpu;
+use std::sync::Arc;
+
+#[cfg_attr(target_os = "macos", derive(Clone))]
+pub struct BladeContext {
+    pub(super) gpu: Arc<gpu::Context>,
+}
+
+impl BladeContext {
+    pub fn new() -> anyhow::Result<Self> {
+        let gpu = Arc::new(
+            unsafe {
+                gpu::Context::init(gpu::ContextDesc {
+                    presentation: true,
+                    validation: false,
+                    ..Default::default()
+                })
+            }
+            .map_err(|e| anyhow::anyhow!("{:?}", e))?,
+        );
+        Ok(Self { gpu })
+    }
+}
